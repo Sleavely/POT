8707ced2f4a8dbe080b4c3ae89a0b1c4bfb3f2b2
wrzasq <wrzasq@c77455bc-2124-0410-8220-ac9e7aafbb94>
2007-11-07 20:48:44 +0000

    Small revdbsys merge fix.

--- classes/OTS_Account.php
+++ classes/OTS_Account.php
@@ -7,7 +7,7 @@
 
 /**
  * @package POT
- * @version 0.0.5
+ * @version 0.0.6+SVN
  * @author Wrzasq <wrzasq@gmail.com>
  * @copyright 2007 (C) by Wrzasq
  * @license http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public License, Version 3
@@ -17,7 +17,7 @@
  * OTServ account abstraction.
  * 
  * @package POT
- * @version 0.0.5
+ * @version 0.0.6+SVN
  */
 class OTS_Account extends OTS_Base_DAO implements IteratorAggregate, Countable
 {
@@ -37,11 +37,7 @@ class OTS_Account extends OTS_Base_DAO implements IteratorAggregate, Countable
  * Remember! This method sets blocked flag to true after account creation!
  * </p>
  * 
- * <p>
- * IMPORTANT: Since 0.0.4 there is group_id field which this method does not support. Account's group_id is set to first one found in database. You should use {@link OTS_Account::createEx() createEx()} method if you want to set group_id field during creation.
- * </p>
- * 
- * @version 0.0.4
+ * @version 0.0.6+SVN
  * @param int $min Minimum number.
  * @param int $max Maximum number.
  * @return int Created account number.
@@ -49,32 +45,6 @@ class OTS_Account extends OTS_Base_DAO implements IteratorAggregate, Countable
  * @throws Exception When there are no free account numbers.
  */
     public function create($min = 1, $max = 9999999)
-    {
-        // loads default group
-        $groups = POT::getInstance()->createObject('Groups_List');
-        $groups->rewind();
-        return $this->createEx( $groups->current(), $min, $max);
-    }
-
-/**
- * Creates new account.
- * 
- * Create new account in given range (1 - 9999999 by default) in given group.
- * 
- * <p>
- * Remember! This method sets blocked flag to true after account creation!
- * </p>
- * 
- * @version 0.0.5
- * @since 0.0.4
- * @param OTS_Group $group Group to be assigned to account.
- * @param int $min Minimum number.
- * @param int $max Maximum number.
- * @return int Created account number.
- * @example examples/create.php account.php
- * @throws Exception When there are no free account numbers.
- */
-    public function createEx(OTS_Group $group, $min = 1, $max = 9999999)
     {
         // generates random account number
         $random = rand($min, $max);
@@ -114,24 +84,49 @@ class OTS_Account extends OTS_Base_DAO implements IteratorAggregate, Countable
 
         // saves blank account info
         $this->data['id'] = $number;
-        $this->data['group_id'] = $group->getId();
         $this->data['blocked'] = true;
 
-        $this->db->query('INSERT INTO ' . $this->db->tableName('accounts') . ' (' . $this->db->fieldName('id') . ', ' . $this->db->fieldName('group_id') . ', ' . $this->db->fieldName('password') . ', ' . $this->db->fieldName('email') . ', ' . $this->db->fieldName('blocked') . ') VALUES (' . $number . ', ' . $this->data['group_id'] . ', \'\', \'\', 1)');
+        $this->db->query('INSERT INTO ' . $this->db->tableName('accounts') . ' (' . $this->db->fieldName('id') . ', ' . $this->db->fieldName('password') . ', ' . $this->db->fieldName('email') . ', ' . $this->db->fieldName('blocked') . ') VALUES (' . $number . ', \'\', \'\', 1)');
 
         return $number;
     }
 
 /**
+ * Creates new account.
+ * 
+ * Create new account in given range (1 - 9999999 by default) in given group.
+ * 
+ * <p>
+ * Remember! This method sets blocked flag to true after account creation!
+ * </p>
+ * 
+ * <p>
+ * IMPORTANT: Since 0.0.6+SVN there isn't group_id field which this method was created for. You should use {@link OTS_Account::create() create()} method.
+ * </p>
+ * 
+ * @version 0.0.6_SVN
+ * @since 0.0.4
+ * @param OTS_Group $group Group to be assigned to account.
+ * @param int $min Minimum number.
+ * @param int $max Maximum number.
+ * @return int Created account number.
+ * @deprecated 0.0.6+SVN There is no more group_id field in database, use create().
+ */
+    public function createEx(OTS_Group $group, $min = 1, $max = 9999999)
+    {
+        return $this->create($min, $max);
+    }
+
+/**
  * Loads account with given number.
  * 
- * @version 0.0.5
+ * @version 0.0.6+SVN
  * @param int $id Account number.
  */
     public function load($id)
     {
         // SELECT query on database
-        $this->data = $this->db->query('SELECT ' . $this->db->fieldName('id') . ', ' . $this->db->fieldName('group_id') . ', ' . $this->db->fieldName('password') . ', ' . $this->db->fieldName('email') . ', ' . $this->db->fieldName('blocked') . ' FROM ' . $this->db->tableName('accounts') . ' WHERE ' . $this->db->fieldName('id') . ' = ' . (int) $id)->fetch();
+        $this->data = $this->db->query('SELECT ' . $this->db->fieldName('id') . ', ' . $this->db->fieldName('password') . ', ' . $this->db->fieldName('email') . ', ' . $this->db->fieldName('blocked') . ' FROM ' . $this->db->tableName('accounts') . ' WHERE ' . $this->db->fieldName('id') . ' = ' . (int) $id)->fetch();
     }
 
 /**
@@ -166,7 +161,7 @@ class OTS_Account extends OTS_Base_DAO implements IteratorAggregate, Countable
 /**
  * Updates account in database.
  * 
- * @version 0.0.5
+ * @version 0.0.6+SVN
  * @throws E_OTS_NotLoaded False if account doesn't have ID assigned.
  */
     public function save()
@@ -177,7 +172,7 @@ class OTS_Account extends OTS_Base_DAO implements IteratorAggregate, Countable
         }
 
         // UPDATE query on database
-        $this->db->query('UPDATE ' . $this->db->tableName('accounts') . ' SET ' . $this->db->fieldName('group_id') . ' = ' . $this->data['group_id'] . ', ' . $this->db->fieldName('password') . ' = ' . $this->db->quote($this->data['password']) . ', ' . $this->db->fieldName('email') . ' = ' . $this->db->quote($this->data['email']) . ', ' . $this->db->fieldName('blocked') . ' = ' . (int) $this->data['blocked'] . ' WHERE ' . $this->db->fieldName('id') . ' = ' . $this->data['id']);
+        $this->db->query('UPDATE ' . $this->db->tableName('accounts') . ' SET ' . $this->db->fieldName('password') . ' = ' . $this->db->quote($this->data['password']) . ', ' . $this->db->fieldName('email') . ' = ' . $this->db->quote($this->data['email']) . ', ' . $this->db->fieldName('blocked') . ' = ' . (int) $this->data['blocked'] . ' WHERE ' . $this->db->fieldName('id') . ' = ' . $this->data['id']);
     }
 
 /**
@@ -200,31 +195,34 @@ class OTS_Account extends OTS_Base_DAO implements IteratorAggregate, Countable
 /**
  * Returns group of this account.
  * 
- * @version 0.0.4
+ * @version 0.0.6+SVN
  * @since 0.0.4
- * @return OTS_Group Group of which current account is member.
+ * @return OTS_Group Group of which current account is member (currently random group).
  * @throws E_OTS_NotLoaded If account is not loaded.
+ * @deprecated 0.0.6+SVN There is no more group_id field in database.
  */
     public function getGroup()
     {
-        if( !isset($this->data['group_id']) )
+        if( !isset($this->data['id']) )
         {
             throw new E_OTS_NotLoaded();
         }
 
-        $group = POT::getInstance()->createObject('Group');
-        $group->load($this->data['group_id']);
-        return $group;
+        // loads default group
+        $groups = POT::getInstance()->createObject('Groups_List');
+        $groups->rewind();
+        return $groups->current();
     }
 
 /**
  * Assigns account to group.
  * 
+ * @version 0.0.6+SVN
  * @param OTS_Group $group Group to be a member.
+ * @deprecated 0.0.6+SVN There is no more group_id field in database.
  */
     public function setGroup(OTS_Group $group)
     {
-        $this->data['group_id'] = $group->getId();
     }
 
 /**
